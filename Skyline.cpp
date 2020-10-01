38dad212398ae7dc321fce04bf21869c75b0f9bc
9021794820 <63342284+9021794820@users.noreply.github.com>
2020-10-01 13:48:58 +0530

    Update Skyline.cpp

--- Skyline.cpp
+++ Skyline.cpp
@@ -1,109 +1,125 @@
-#include <stdio.h>
-#include <stdlib.h>
-typedef struct d{
-        int p,y,g;
-       } d;
-int min(d* a,int size)
-{
-  int p_min = a[0].p;     
-    int i;
-    for(i=0;i<size;i++)
-    {
-      if(a[i].p < p_min)
-      {
-                 p_min = a[i].p;                   
-                ;
-      }       
-    }
-    return p_min;
-    
-}
-int max(d* a,int size)
-{
-  int p_max = a[0].p + a[0].g ;     
-    int i;
-    for(i=0;i<size;i++)
-    {
-      if(a[i].p + a[i].g >= p_max)
-      {
-                 p_max = a[i].p + a[i].g;                   
-                 
-      }       
-    }
-    return p_max;
-    
-}
-int mutlak(int a,int b)
-{
-  if(a>b)
-  return a-b;
-  else
-  return b-a;      
-}
-int main(int argc, char *argv[])
-{
-    d* x; int a=0;
-    x = (d*)malloc(sizeof(d));
-    int *dizi;
-     int i,j,cevre=0,bi=0;
-    
-    do{
-        
-        scanf("%d",&(x[a].p));
-        if (x[a].p==-1)
-        break;
-        scanf("%d%d",&(x[a].y),&(x[a].g));
-        x = (d*)realloc(x,sizeof(d)*(a+2));     
-             a++;
-    }while(x[a].p != -1);
-    int boyut = max(x,a) - min(x,a)+x[0].p+1 ;
-   dizi = (int*)calloc(  sizeof(int), boyut);
-   
-   for(i=1;i<boyut;i++)
-   {
-       for(j=0;j<a;j++){
-          if(i>=x[j].p && i< (x[j].p + x[j].g) )
-          {
-                  if(x[j].y > dizi[i])
-                  dizi[i] =   x[j].y;   
-                       
-          }                
-                           
-       }             
-                       
-   }
-   dizi[boyut+1] = 0;
-   for(i=1;i<boyut;i++)
-   {
-       if(dizi[i] != dizi[i-1])
-            {
-                    cevre +=  mutlak(dizi[i], dizi[i-1]);   
-                    if(dizi[bi] != 0 )     
-                    cevre += i-bi;
-                    bi = i;
-            }     
-   }
-    for(i=0;i<boyut;i++)
-    {     printf(" %d ",dizi[i]);                                
-    }
-    printf("cevre = %d ",cevre);
-  system("PAUSE");	return 0;}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// Function to create skyline 
+vector<pair<int, int> > 
+createSkyline(vector<vector<int> >& buildings) 
+{ 
+  
+    // Get the number of buildings 
+    int N = buildings.size(); 
+  
+    // To store the left and right 
+    // wall position of the buildings 
+    vector<pair<int, int> > wall; 
+  
+    // Triplet of building structure 
+    // parameters 
+    int left, height, right; 
+    for (int i = 0; i < N; i++) { 
+  
+        // Get left point of building 
+        left = buildings[i][0]; 
+  
+        // Get height of building 
+        height = buildings[i][1]; 
+  
+        // Get right point of building 
+        right = buildings[i][2]; 
+  
+        // Store left point and height 
+        // of the left wall 
+  
+        // Negative value means left wall 
+        // will be inserted to multiset first 
+        // for the same abscissa(x) as right wall 
+        wall.push_back({ left, -height }); 
+  
+        // Store right point and height 
+        // of the right wall 
+        wall.push_back( 
+            make_pair(right, height)); 
+    } 
+  
+    // Sort the walls in ascending order 
+    sort(wall.begin(), wall.end()); 
+  
+    // To store skyline: output 
+    vector<pair<int, int> > skyline; 
+  
+    // Initialize a multiset to 
+    // keep left wall heights sorted 
+    multiset<int> leftWallHeight = { 0 }; 
+  
+    // Current max height among 
+    // leftWallHeights 
+    int top = 0; 
+  
+    // Traverse through the sorted walls 
+    for (auto w : wall) { 
+  
+        // If left wall is found 
+        if (w.second < 0) { 
+  
+            // Insert the height 
+            leftWallHeight.insert(-w.second); 
+        } 
+  
+        // If right wall is found 
+        else { 
+  
+            // Remove the height 
+            leftWallHeight.erase( 
+                leftWallHeight.find(w.second)); 
+        } 
+  
+        // Mark a skyline point if top changes 
+        // .rbegin(): reverse iterator 
+        if (*leftWallHeight.rbegin() != top) { 
+  
+            top = *leftWallHeight.rbegin(); 
+            skyline.push_back( 
+                make_pair(w.first, top)); 
+        } 
+    } 
+  
+    // Return skyline to printSkyline 
+    return skyline; 
+} 
+  
+// Function to print the output skyline 
+void printSkyline( 
+    vector<vector<int> >& buildings) 
+{ 
+  
+    // Function call for creating skyline 
+    vector<pair<int, int> > skyline 
+        = createSkyline(buildings); 
+  
+    cout << "Skyline for given"
+         << " buildings:\n{"; 
+  
+    for (auto it : skyline) { 
+  
+        cout << "{" << it.first << ", "
+             << it.second << "} "; 
+    } 
+    cout << "}"; 
+} 
+  
+// Driver Code 
+int main() 
+{ 
+    vector<vector<int> > buildings;blockquote 
+  
+    // Given left and right location 
+    // and height of the wall 
+    buildings = { { 1, 11, 5 }, { 2, 6, 7 },  
+                  { 3, 13, 9 }, { 12, 7, 16 }, 
+                  { 14, 3, 25 }, { 19, 18, 22 }, 
+                  { 23, 13, 29 }, { 24, 4, 28 } }; 
+  
+    // Function Call 
+    printSkyline(buildings); 
+    return 0; 
+} 
